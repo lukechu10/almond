5a013df9dcafe2870fc0c1d544163b4df0468509
Luke Chu <lukewchu@outlook.com>
2020-11-05 08:36:56 -0800

    Unescape unicode in identifier

--- src/parser/identifier.rs
+++ src/parser/identifier.rs
@@ -17,8 +17,8 @@ fn parse_unicode_escape_sequence(s: Span) -> IResult<Span, char> {
 }
 
 pub fn parse_identifier(s: Span) -> IResult<Span, Node> {
-    fn parse_identifier_start(s: Span) -> IResult<Span, ()> {
-        let (s, _) = verify(
+    fn parse_identifier_start(s: Span) -> IResult<Span, char> {
+        let (s, c) = verify(
             alt((parse_unicode_escape_sequence, none_of(""))),
             |c: &char| match c {
                 c if unicode_xid::UnicodeXID::is_xid_start(*c) => true,
@@ -26,10 +26,10 @@ pub fn parse_identifier(s: Span) -> IResult<Span, Node> {
                 _ => false,
             },
         )(s)?;
-        Ok((s, ()))
+        Ok((s, c))
     }
-    fn parse_identifier_continue(s: Span) -> IResult<Span, ()> {
-        let (s, _) = verify(
+    fn parse_identifier_continue(s: Span) -> IResult<Span, char> {
+        let (s, c) = verify(
             alt((parse_unicode_escape_sequence, none_of(""))),
             |c: &char| match c {
                 c if unicode_xid::UnicodeXID::is_xid_continue(*c) => true,
@@ -37,23 +37,17 @@ pub fn parse_identifier(s: Span) -> IResult<Span, Node> {
                 _ => false,
             },
         )(s)?;
-        Ok((s, ()))
+        Ok((s, c))
     }
 
     let (s, start) = position(s)?;
-    let (s, name) = recognize(pair(
-        parse_identifier_start,
-        many0(parse_identifier_continue),
-    ))(s)?;
+    let (s, name) = map(
+        pair(parse_identifier_start, many0(parse_identifier_continue)),
+        |(c, v)| format!("{}{}", c, v.into_iter().collect::<String>()),
+    )(s)?;
     let (s, end) = position(s)?;
 
-    Ok((
-        s,
-        NodeKind::Identifier {
-            name: name.fragment().to_string(),
-        }
-        .with_pos(start, end),
-    ))
+    Ok((s, NodeKind::Identifier { name }.with_pos(start, end)))
 }
 
 #[cfg(test)]
@@ -65,11 +59,29 @@ mod tests {
         all_consuming(parse_identifier)("myVar".into()).unwrap();
         all_consuming(parse_identifier)("abc123".into()).unwrap();
         all_consuming(parse_identifier)("$".into()).unwrap();
+        all_consuming(parse_identifier)("_".into()).unwrap();
         all_consuming(parse_identifier)("$elem".into()).unwrap();
+        all_consuming(parse_identifier)("_elem".into()).unwrap();
         all_consuming(parse_identifier)("$123".into()).unwrap();
+        all_consuming(parse_identifier)("_123".into()).unwrap();
 
-        all_consuming(parse_identifier)(r#"abc\u0061"#.into()).unwrap();
-
+        // bad identifier
         all_consuming(parse_identifier)("123abc".into()).unwrap_err();
+    }
+
+    #[test]
+    fn test_identifier_with_unicode_escape() {
+        assert_eq!(
+            parse_identifier(r#"abc\u0061"#.into()).unwrap().1.kind,
+            NodeKind::Identifier {
+                name: "abca".into()
+            }
+        );
+        assert_eq!(
+            parse_identifier(r#"abc\u0061\u0062123"#.into()).unwrap().1.kind,
+            NodeKind::Identifier {
+                name: "abcab123".into()
+            }
+        );
     }
 }
