f44c764cd685829893e03cc799e7b98bfa055791
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-11-04 22:39:42 -0800

    Allow parsing identifiers with unicode and unicode escapes

--- src/parser/identifier.rs
+++ src/parser/identifier.rs
@@ -1,41 +1,56 @@
 use crate::ast::*;
 use crate::parser::util::*;
 use crate::parser::*;
-use nom::{branch::alt, bytes::complete::*, character::complete::*, combinator::*, IResult};
+use nom::{
+    branch::alt, bytes::complete::*, character::complete::*, character::*, combinator::*,
+    number::complete::*, IResult,
+};
 use nom_locate::position;
 
-#[must_use]
+/// Parses any 4 hex digits unicode escape sequence
+fn parse_unicode_escape_sequence(s: Span) -> IResult<Span, char> {
+    let (s, _) = tag("\\u")(s)?;
+    let (s, hex_str) = recognize(count(one_of("1234567890abcdefABCDEF"), 4))(s)?;
+    let hex_u32 = u32::from_str_radix(*hex_str, 16).unwrap(); // FIXME
+    let char = std::char::from_u32(hex_u32).unwrap(); // FIXME
+    Ok((s, char))
+}
+
 pub fn parse_identifier(s: Span) -> IResult<Span, Node> {
-    fn is_identifier_start(c: char) -> bool {
-        match c {
-            c if c.is_alphabetic() => true,
-            '$' | '_' => true,
-            // TODO: if c codepoint is above 128, check c is non ascii identifier start
-            // TODO: check if c is in astral_set
-            _ => false,
-        }
+    fn parse_identifier_start(s: Span) -> IResult<Span, ()> {
+        let (s, _) = verify(
+            alt((parse_unicode_escape_sequence, none_of(""))),
+            |c: &char| match c {
+                c if unicode_xid::UnicodeXID::is_xid_start(*c) => true,
+                '$' | '_' => true,
+                _ => false,
+            },
+        )(s)?;
+        Ok((s, ()))
     }
-    fn is_identifier_char(c: char) -> bool {
-        match c {
-            c if c.is_alphanumeric() => true,
-            '$' | '_' => true,
-            // TODO: if c codepoint is above 128, check c is non ascii identifier start
-            // TODO: check if c is in astral_set
-            _ => false,
-        }
+    fn parse_identifier_continue(s: Span) -> IResult<Span, ()> {
+        let (s, _) = verify(
+            alt((parse_unicode_escape_sequence, none_of(""))),
+            |c: &char| match c {
+                c if unicode_xid::UnicodeXID::is_xid_continue(*c) => true,
+                '$' | '_' => true,
+                _ => false,
+            },
+        )(s)?;
+        Ok((s, ()))
     }
 
     let (s, start) = position(s)?;
-
-    let (s, first) = satisfy(is_identifier_start)(s)?;
-    let (s, rest) = take_while(is_identifier_char)(s)?;
-
+    let (s, name) = recognize(pair(
+        parse_identifier_start,
+        many0(parse_identifier_continue),
+    ))(s)?;
     let (s, end) = position(s)?;
 
     Ok((
         s,
         NodeKind::Identifier {
-            name: format!("{}{}", first, rest),
+            name: name.fragment().to_string(),
         }
         .with_pos(start, end),
     ))
@@ -52,6 +67,8 @@ mod tests {
         all_consuming(parse_identifier)("$".into()).unwrap();
         all_consuming(parse_identifier)("$elem".into()).unwrap();
         all_consuming(parse_identifier)("$123".into()).unwrap();
+
+        all_consuming(parse_identifier)(r#"abc\u0061"#.into()).unwrap();
 
         all_consuming(parse_identifier)("123abc".into()).unwrap_err();
     }
