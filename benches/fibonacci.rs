589881aabd699e9d820e2271746f75305d16809a
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-11-23 21:10:31 -0800

    Fix wrong naming in benches

--- benches/fibonacci.rs
+++ benches/fibonacci.rs
@@ -3,14 +3,14 @@
 
 use criterion::{criterion_group, criterion_main, Criterion};
 
-fn parse_fibonacci() {
+fn f() {
     use coconut::parser::parse_program;
     let program_str = include_str!("./js/fibonacci.js");
     parse_program(program_str.into()).unwrap().1;
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("fibonacci", |b| b.iter(|| parse_fibonacci()));
+    c.bench_function("fibonacci", |b| b.iter(f));
 }
 
 criterion_group!(benches, criterion_benchmark);
--- benches/jquery-v3-5-1.rs
+++ benches/jquery-v3-5-1.rs
@@ -1,13 +1,13 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 
-fn parse_jquery() {
+fn f() {
     use coconut::parser::parse_program;
     let program_str = include_str!("./js/jquery-3.5.1.js");
     parse_program(program_str.into()).unwrap().1;
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("jquery v3.5.1", |b| b.iter(|| parse_jquery()));
+    c.bench_function("jquery v3.5.1", |b| b.iter(f));
 }
 
 criterion_group!(benches, criterion_benchmark);
