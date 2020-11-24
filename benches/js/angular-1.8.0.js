c97b9429912cac27905b15405fa2bd00e9231061
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-11-23 22:18:30 -0800

    Fix remove debug annotations in angular fixture

--- benches/js/angular-1.8.0.js
+++ benches/js/angular-1.8.0.js
@@ -3,7 +3,7 @@
  * (c) 2010-2020 Google, Inc. http://angularjs.org
  * License: MIT
  */
-// (function(window) {'use strict';
+(function(window) {'use strict';
 
 /* exported
   minErrConfig,
@@ -36491,6 +36491,6 @@ $provide.value("$locale", {
     angularInit(window.document, bootstrap);
   });
 
-// })(window);
+})(window);
 
 !window.angular.$$csp().noInlineStyle && window.angular.element(document.head).prepend(window.angular.element('<style>').text('@charset "UTF-8";[ng\\:cloak],[ng-cloak],[data-ng-cloak],[x-ng-cloak],.ng-cloak,.x-ng-cloak,.ng-hide:not(.ng-hide-animate){display:none !important;}ng\\:form{display:block;}.ng-animate-shim{visibility:hidden;}.ng-anchor{position:absolute;}'));
